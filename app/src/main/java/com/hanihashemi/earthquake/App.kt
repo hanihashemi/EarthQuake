99cac6c809e312d4f529fc98c4a5068d7f0d1b9f
Hani Hashemi <hani.hashemi@zoodfood.com>
2018-08-08 13:13:12 +0300

    Add timber library and App

---
+++ app/src/main/java/com/hanihashemi/earthquake/App.kt
@@ -0,0 +1,34 @@
+package com.hanihashemi.earthquake
+
+import android.app.Application
+import android.util.Log
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+
+
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        initTimber()
+    }
+
+    private fun initTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        } else {
+            Timber.plant(CrashReportingTree())
+        }
+    }
+
+    private class CrashReportingTree : Timber.Tree() {
+        override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+            if (priority == Log.VERBOSE || priority == Log.DEBUG || priority == Log.INFO)
+                return
+
+//            if (t != null)
+//                Crashlytics.logException(t)
+        }
+    }
+}
