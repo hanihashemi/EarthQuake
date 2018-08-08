e0dba78898bfddccfe554dd331f687cb2726a724
Hani Hashemi <hani.hashemi@zoodfood.com>
2018-08-08 12:39:31 +0300

    Add MapActivity Remove MainActivity

---
+++ app/src/main/java/com/hanihashemi/earthquake/MapsActivity.kt
@@ -0,0 +1,35 @@
+package com.hanihashemi.earthquake
+
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.hanihashemi.earthquake.base.BaseTransparentActivity
+
+class MapsActivity : BaseTransparentActivity(), OnMapReadyCallback {
+
+    private lateinit var map: GoogleMap
+
+    override val layoutResource
+        get() = R.layout.activity_maps
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+
+        val sydney = LatLng(-34.0, 151.0)
+        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+}
