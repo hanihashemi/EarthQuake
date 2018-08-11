7ccbdb0de56e5851b1e8c79f2ea9348316cfd65a
Hani Hashemi <hani.hashemi@zoodfood.com>
2018-08-11 20:00:41 +0300

    load data from db

--- app/src/main/java/com/hanihashemi/earthquake/data/repository/EventRepository.kt
+++ app/src/main/java/com/hanihashemi/earthquake/data/repository/EventRepository.kt
@@ -1,12 +1,12 @@
 package com.hanihashemi.earthquake.data.repository
 
 import android.arch.lifecycle.LiveData
-import android.arch.lifecycle.MutableLiveData
-import com.hanihashemi.earthquake.data.db.ApiResponse
+import android.arch.lifecycle.MediatorLiveData
 import com.hanihashemi.earthquake.data.db.FeatureDao
-import com.hanihashemi.earthquake.data.db.NetworkBoundResource
-import com.hanihashemi.earthquake.data.db.Resource
+import com.hanihashemi.earthquake.data.network.ApiResponse
 import com.hanihashemi.earthquake.data.network.EventService
+import com.hanihashemi.earthquake.data.network.NetworkBoundResource
+import com.hanihashemi.earthquake.data.network.Resource
 import com.hanihashemi.earthquake.model.FeatureCollection
 import com.hanihashemi.earthquake.util.AppExecutors
 
@@ -16,24 +16,26 @@ class EventRepository(
 
     fun get(): LiveData<Resource<FeatureCollection>> {
         return object : NetworkBoundResource<FeatureCollection, FeatureCollection>(AppExecutors()) {
-            override fun saveCallResult(item: FeatureCollection) {
-                featureDao.insert(*item.features.toTypedArray())
-            }
+            override fun saveCallResult(item: FeatureCollection) =
+                    featureDao.insert(*item.features.toTypedArray())
 
-            override fun shouldFetch(data: FeatureCollection?): Boolean {
-                return true
-            }
+            override fun shouldFetch(data: FeatureCollection?) = true
 
             override fun loadFromDb(): LiveData<FeatureCollection> {
-                val mutableLiveData = MutableLiveData<FeatureCollection>()
-                mutableLiveData.value = FeatureCollection(listOf())
-                return mutableLiveData
+                val features = featureDao.loadAll()
+
+                val featureCollection = MediatorLiveData<FeatureCollection>()
+                featureCollection.addSource(features) {
+                    if (it != null)
+                        featureCollection.value = FeatureCollection(it)
+                    else
+                        featureCollection.value = FeatureCollection(listOf())
+                }
+
+                return featureCollection
             }
 
-            override fun createCall(): LiveData<ApiResponse<FeatureCollection>> {
-                return eventService.get()
-            }
-
+            override fun createCall(): LiveData<ApiResponse<FeatureCollection>> = eventService.get()
         }.asLiveData()
     }
 }
