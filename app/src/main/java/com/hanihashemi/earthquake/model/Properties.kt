2d85e9776aca98e9193cdad10599638e646ed7d1
Hani Hashemi <hani.hashemi@zoodfood.com>
2018-08-14 15:50:24 +0300

    get country code and write test for it

--- app/src/main/java/com/hanihashemi/earthquake/model/Properties.kt
+++ app/src/main/java/com/hanihashemi/earthquake/model/Properties.kt
@@ -1,6 +1,6 @@
 package com.hanihashemi.earthquake.model
 
-import android.text.format.DateUtils
+import com.neovisionaries.i18n.CountryCode
 
 data class Properties(
         val mag: Float,
@@ -9,5 +9,13 @@ data class Properties(
         val time: Long
 ) {
     val city: String
-        get() = title.substring(title.indexOf("of") + 3)
+        get() = title.substring(title.lastIndexOf("of") + 3)
+
+    val countryCode: String
+        get() {
+            val countryName = title.substring(title.lastIndexOf(",") + 2)
+            val abbreviations = CountryCode.findByName(countryName)
+
+            return if (abbreviations.size == 0) "us" else abbreviations.first().name
+        }
 }
---
+++ app/src/test/java/com/hanihashemi/earthquake/model/PropertiesTest.kt
@@ -0,0 +1,31 @@
+package com.hanihashemi.earthquake.model
+
+import org.junit.Assert.*
+import org.junit.Test
+import java.util.*
+
+
+class PropertiesTest{
+
+    @Test
+    fun country_code(){
+        val countries = hashMapOf<String, String>()
+
+        var properties = Properties(0F, "", "M 1.6 - 67km N of Tonopah, Nevada", 222L)
+        assertEquals("us", properties.countryCode)
+
+        properties = Properties(0F, "", "M 1.6 - 67km N of Tonopah, Alaska", 222L)
+        assertEquals("us", properties.countryCode)
+
+        properties = Properties(0F, "", "M 1.6 - 67km N of Tonopah, US", 222L)
+        assertEquals("us", properties.countryCode)
+
+        properties = Properties(0F, "", "M 1.6 - 67km N of Tonopah, Canada", 222L)
+        assertEquals("CA", properties.countryCode)
+
+        properties = Properties(0F, "", "M 1.6 - 67km N of Tonopah, Turkey", 222L)
+        assertEquals("TR", properties.countryCode)
+    }
+
+
+}
